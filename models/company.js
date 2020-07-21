9c985da4478389248da7c4b146b3806107746094
Leo <medinayleo@gmail.com>
2020-07-20 21:07:14 -0400

    Completed company routes, middleware, and model functions

--- models/company.js
+++ models/company.js
@@ -1,23 +1,19 @@
-const db = require("../db");
-
 /** Collection of methods related to companies
  * 
  * Functions
  *  -> Create   - Creates new company in database.
- *  -> FindAll  - (TDB)
- *  -> FindOne  - (TDB)
- *  -> Update   - (TDB)
+ *  -> Remove   - Deletes a single company from the database.
+ *  -> FindAll  - Retrieves all companies in database.
+ *  -> get      - Retrieves a single company in database.
+ *  -> Update   - Updates a single company in database.
  * 
 */
 
+const db = require("../db");
+
 class Company {
-  /**  
-   * The following function creates a new company in the database.
-   * 
-   * Params: { handle, name, num_employees, description, logo_url }
-   * Returns: { handle, name, num_employees, description, logo_url }
-   * 
-   */
+
+  /** Creates a new company. Params: { handle, name, num_employees, description, logo_url } */
   static async create(data) {
     const result = await db.query(`
       INSERT INTO companies (
@@ -39,6 +35,21 @@ class Company {
     return result.rows[0];
   };
 
+  /** Deletes a single company from the database */
+
+  static async remove(handle) {
+    const result = await db.query(`
+      DELETE FROM companies
+        WHERE handle = $1
+        RETURNING handle
+    `, [handle]);
+
+    if (result.rowCount === 0) {
+      throw { message: `There is no company with handle ${handle}`, status: 400 }
+    }
+  };
+
+  /** Retrieve all companies in the database */
   static async findAll() {
     const results = await db.query(`
       SELECT handle,
@@ -48,10 +59,60 @@ class Company {
              logo_url
         FROM companies
         ORDER BY handle
-    `)
-
+    `);
     return results.rows;
-  }
-}
+  };
+
+  /** Retrieves a single company from the database */
+  static async get(handle) {
+    const result = await db.query(`
+      SELECT handle,
+             name,
+             num_employees,
+             description,
+             logo_url
+      FROM companies
+      WHERE handle=$1
+      ORDER BY handle 
+    `, [handle]);
+
+    if (result.rowCount === 0) {
+      throw { message: `No company with handle: ${handle}`, status: 400 }
+    };
+
+    return result.rows[0];
+  };
+
+  /** Updates a single company in teh database */
+  static async update(handle, data) {
+    const result = await db.query(`
+      UPDATE companies SET
+        handle=($1),
+        name=($2),
+        num_employees=($3),
+        description=($4),
+        logo_url=($5)
+      WHERE handle=($6)
+      RETURNING handle,
+                name,
+                num_employees,
+                description,
+                logo_url
+    `,
+      [
+        data.handle,
+        data.name,
+        data.num_employees,
+        data.description,
+        data.logo_url,
+        handle
+      ]);
+
+    if (result.rowCount === 0) {
+      throw { message: `No company with handle: ${handle}`, status: 400 }
+    };
+    return result.rows[0];
+  };
+};
 
 module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -1,25 +1,31 @@
+/** Collection of routes for "/companies"
+ *
+ * Routes
+ *  -> GET    "/"       - Returns all companies.
+ *  -> POST   "/"       - Creates new company.
+ *  -> GET    "/handle" - Returns single company.
+ *  -> PUT    "/handle" - Updates single company.
+ *  -> DELETE "/handle" - Deletes single company.
+ *
+*/
+
 const express = require("express");
 const Company = require("../models/company");
 const valCompanySchema = require("../middleware/schemaValidators");
 
 const router = new express.Router();
 
-/** GET / Companies 
- * -> returns all companies in database 
- * -> {companies: [company, ...]}
- */
+/** GET / Companies -> { companies: [company, ...] } */
 router.get("/", async function (req, res, next) {
   try {
     const companies = await Company.findAll();
-    return res.json({ companies });
+    return res.status(200).json({ companies });
   } catch (error) {
     return next(error);
   };
 });
 
-/** POST /companies -> creates new company in database
- *  -> returns created company or validation errors
- */
+/** POST /companies -> returns created company */
 router.post("/", valCompanySchema, async function (req, res, next) {
   try {
     const company = await Company.create(req.body);
@@ -29,6 +35,34 @@ router.post("/", valCompanySchema, async function (req, res, next) {
   }
 });
 
+/** GET / Companies / handle -> { company: [handle, ...] } */
+router.get("/:handle", async function (req, res, next) {
+  try {
+    const company = await Company.get(req.params.handle);
+    return res.status(200).json({ company });
+  } catch (error) {
+    return next(error);
+  };
+});
 
+/** PUT / companies / handle -> { company: [handle, ...] } */
+router.put("/:handle", valCompanySchema, async function (req, res, next) {
+  try {
+    const company = await Company.update(req.params.handle, req.body);
+    return res.status(200).json({ company });
+  } catch (error) {
+    return next(error);
+  };
+});
+
+/** DELETE / Companies / handle -> { message: "Company deleted" } */
+router.delete("/:handle", async function (req, res, next) {
+  try {
+    await Company.remove(req.params.handle);
+    return res.status(200).json({ message: "company deleted" })
+  } catch (error) {
+    return next(error);
+  };
+});
 
 module.exports = router;
