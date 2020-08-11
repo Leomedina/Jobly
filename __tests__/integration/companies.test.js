1f4bef516a0cd01ff15476a1440afdd91093e396
Leo <medinayleo@gmail.com>
2020-08-11 05:46:09 -0400

    finished testing all CRUD funcitons on companies

--- __tests__/integration/companies.test.js
+++ __tests__/integration/companies.test.js
@@ -1,8 +1,6 @@
 const request = require("supertest");
-const jwt = require("jsonwebtoken");
 const app = require("../../app");
 const db = require("../../db");
-const { notify } = require("../../app");
 const VALID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsQG5vdC1yZWFsLmNvbSIsImlhdCI6MTU5NzEzMDcxNX0.1MSF8_KZsELTBbiEAAX720P5dfTQ2VB_XGcmx3OU75I"
 
 describe("Test getting companies from company routes", function () {
@@ -43,7 +41,7 @@ describe("Test adding companies to company routes", function () {
       .post("/companies")
       .send({
         _token: VALID_TOKEN,
-        handle: "fake12",
+        handle: "fake123",
         name: "Fake_Apple",
         num_employees: 100000,
         description: "Hardware and Software",
@@ -54,7 +52,40 @@ describe("Test adding companies to company routes", function () {
   });
 });
 
-describe("Get a specific company", async function())
+describe("Test updating company", function () {
+  it("should delete a company", async function () {
+    let response = await request(app)
+      .patch("/companies/fake123")
+      .send({
+        _token: VALID_TOKEN,
+        name: "Fake_e"
+      });
+
+    expect(response.statusCode).toEqual(200);
+  });
+});
+
+describe("Test delete company", function () {
+  it("should delete a company", async function () {
+    let response = await request(app)
+      .delete("/companies/fake123")
+      .send({
+        _token: VALID_TOKEN
+      });
+
+    expect(response.statusCode).toEqual(200);
+  });
+});
+
+describe("Get a specific company", async function () {
+  it("should display a specific company in db", async function () {
+    let response = await request(app)
+      .get("/companies/apple")
+      .send({ _token: VALID_TOKEN });
+
+    expect(response.statusCode).toEqual(200);
+  });
+})
 
 afterAll(async function () {
   await db.end();
